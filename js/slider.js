0146111891b98053845e8195d6de086fad0d7a20
pwrq <pwrdpwnz@gmail.com>
2022-09-08 22:04:54 +0300

    fix responsive

--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,69 @@
-let slides = document.querySelectorAll('#slides .slider__slide');
-let next = document.getElementById('next');
-let previous = document.getElementById('prev');
-let currentSlide = 0;
+// let slides = document.querySelectorAll('#slides .slider__slide');
+// let next = document.getElementById('next');
+// let previous = document.getElementById('prev');
+// let currentSlide = 0;
+//
+// function nextSlide() {
+//   goToSlide(currentSlide+1);
+// }
+//
+// function previousSlide() {
+//   goToSlide(currentSlide-1);
+// }
+//
+// function goToSlide(n) {
+//   slides[currentSlide].className = 'slider__slide';
+//   currentSlide = (n+slides.length)%slides.length;
+//   slides[currentSlide].className = 'slider__slide showing';
+// }
+//
+// next.onclick = function() {
+//   nextSlide();
+// };
+// previous.onclick = function() {
+//   previousSlide();
+// };
 
-function nextSlide() {
-  goToSlide(currentSlide+1);
-}
+$('#owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+    0:{
+      items:1,
+      nav:true
+    },
+    600:{
+      items:2,
+      nav:false
+    },
+    1000:{
+      items:2,
+      nav:true,
+      loop:false
+    }
+  }
+})
 
-function previousSlide() {
-  goToSlide(currentSlide-1);
-}
 
-function goToSlide(n) {
-  slides[currentSlide].className = 'slider__slide';
-  currentSlide = (n+slides.length)%slides.length;
-  slides[currentSlide].className = 'slider__slide showing';
-}
+$('#owl-carousel-2').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+    0:{
+      items:1,
+      nav:true
+    },
+    600:{
+      items:3,
+      nav:false
+    },
+    1000:{
+      items:3,
+      nav:true,
+      loop:false
+    }
+  }
+})
 
-next.onclick = function() {
-  nextSlide();
-};
-previous.onclick = function() {
-  previousSlide();
-};
